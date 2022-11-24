e377c9972c61f5799731b40d5f4f098104b94ea8
$BlueDev5 <gamestudio275@gmail.com>
2022-11-24 18:54:41 +0530

    feat: :sparkles: Add Enemy AI, it follows and shoots the player

---
+++ Assets/Scripts/Enemy/EnemyAi.cs
@@ -0,0 +1,132 @@
+using System;
+using UnityEngine;
+
+
+namespace Game.Enemy
+{
+    public class EnemyAi : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] private float _speed;
+        [SerializeField] private float _distance;
+        [SerializeField] private Transform _wallDetector;
+        [SerializeField] private LayerMask _whatIsGround;
+        [SerializeField] private LayerMask _whatIsWall;
+        [SerializeField] private LayerMask _whatIsPlayer;
+
+        [Header("Attack")]
+        [SerializeField] private float _shootingRange;
+        [SerializeField] private float _timeBetweenAttacks;
+        [SerializeField] private Projectile _projectile;
+        [SerializeField] private Transform _projectileShootPoint;
+
+        private bool _movingRight = false;
+        private bool _isShooting = false;
+
+        private float _timeSinceLastAttack = 0f;
+
+        private Player.PlayerController _player;
+
+        #endregion
+
+
+        #region Getters and Setters
+
+        #endregion
+
+
+        #region Unity Calls
+        void Flip()
+        {
+            _movingRight = !_movingRight;
+            transform.eulerAngles = new Vector3(0, _movingRight ? 180 : 0, 0);
+        }
+
+        void Update()
+        {
+            _isShooting = PlayerInShootingRange();
+            if (!_isShooting)
+            {
+                _timeSinceLastAttack = 0;
+            }
+
+            if (_isShooting && _timeSinceLastAttack >= _timeBetweenAttacks)
+            {
+                Shoot();
+            }
+
+            if (ObstacleAhead())
+            {
+                Flip();
+            }
+
+            FacePlayerIfShooting();
+
+            if (!PlayerAhead())
+            {
+                transform.Translate(Vector2.left * _speed * Time.deltaTime);
+            }
+
+            _timeSinceLastAttack += Time.deltaTime;
+        }
+
+        private void FacePlayerIfShooting()
+        {
+            if (!_isShooting) return;
+
+            if (_movingRight && transform.position.x > _player.transform.position.x)
+            {
+                Flip();
+            }
+            else if (!_movingRight && transform.position.x < _player.transform.position.x)
+            {
+                Flip();
+            }
+        }
+
+        void Awake()
+        {
+            _player = GameObject.FindObjectOfType<Player.PlayerController>();
+        }
+        #endregion
+
+
+        #region Functions
+        private bool PlayerInShootingRange()
+        {
+            var xDistance = Mathf.Abs(_player.transform.position.x - transform.position.x);
+            var yDistance = Mathf.Abs(_player.transform.position.y - transform.position.y);
+
+            return xDistance < _shootingRange && yDistance < 0.75f;
+        }
+
+        private bool ObstacleAhead()
+        {
+            var obstacleInfo = Physics2D.Raycast(_wallDetector.position, _movingRight ? Vector2.right : Vector2.left, _distance, _whatIsWall);
+            return obstacleInfo.collider != null;
+        }
+
+        private bool PlayerAhead()
+        {
+
+            var playerInfo = Physics2D.Raycast(_wallDetector.position, _movingRight ? Vector2.right : Vector2.left, _distance, _whatIsPlayer);
+            return playerInfo.collider != null;
+        }
+
+        private bool IsOnGround()
+        {
+            var groundInfo = Physics2D.Raycast(_wallDetector.position, Vector2.down, 0.1f, _whatIsGround);
+            return groundInfo.collider != null;
+        }
+
+        private void Shoot()
+        {
+            _timeSinceLastAttack = 0;
+
+            // * Add Logic to shoot the projectile.
+            var projectile = Instantiate(_projectile, _projectileShootPoint.position, Quaternion.identity);
+            projectile.Direction = new Vector2(_movingRight ? 1 : -1, 0);
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+
+namespace Game.Enemy
+{
+    public class Projectile : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] private float _speed;
+        public Vector2 Direction;
+        [SerializeField] private LayerMask _whatIsObstacle;
+        #endregion
+
+
+        #region Getters and Setters
+
+        #endregion
+
+
+        #region Unity Calls
+        void Update()
+        {
+            transform.Translate(Direction * _speed * Time.deltaTime);
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if ((_whatIsObstacle.value & (1 << other.gameObject.layer)) > 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Awake()
+        {
+            Destroy(gameObject, 2f);
+        }
+        #endregion
+
+
+        #region Functions
+
+        #endregion
+    }
+}
